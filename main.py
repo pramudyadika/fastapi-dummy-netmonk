02953765163f5526f01f4aef20c7178f772e1c8e
pramudyadika <pramudyadika.pd@mail.ugm.ac.id>
2024-02-27 17:35:09 +0700

    feat: add get all/single post

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from model import PostSchema
+
+posts = [
+    {
+        "id": 1,
+        "title": "some title",
+        "content": "some content"
+    },
+    {
+        "id": 2,
+        "title": "another title",
+        "content": "another content"
+    },
+    {
+        "id": 3,
+        "title": "yet another title",
+        "content": "yet another content"
+    }
+
+]
+
+users = []
+
+app = FastAPI()
+
+# Get for testing
+@app.get("/", tags=["Test"])
+async def greet():
+    return {"Hello": "World"}
+
+# Get all posts
+@app.get("/posts", response_model=PostSchema, tags=["Posts"])
+async def get_posts():
+    return {"data": posts}
+
+# Get a single post
+@app.get("/posts/{id}", response_model=PostSchema, tags=["Posts"])
+async def get_one_post(id: int):
+    for post in posts:
+        if post["id"] == id:
+            return {"data": post}
+    return {"error": "Post not found"}
